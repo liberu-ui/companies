3fe0514f92339dc71ab5a9d53d18fb764c80db87
mishu <mishu@xtelecom.ro>
2021-11-12 11:31:59 +0200

    updates eslint

--- src/bulma/pages/companies/Edit.vue
+++ src/bulma/pages/companies/Edit.vue
@@ -1,37 +1,31 @@
 <template>
     <div class="columns is-centered">
         <div class="column is-three-quarters is-full-touch">
-            <enso-form
-class="box has-background-light raises-on-hover"
+            <enso-form class="box has-background-light raises-on-hover"
                 ref="form">
                 <template #mandatary="props">
-                    <form-field
-v-bind="props"
+                    <form-field v-bind="props"
                         :pivot-params="pivotParams"/>
                 </template>
             </enso-form>
             <accessories>
                 <template #default="{ count }">
-                    <tab
-keep-alive
+                    <tab keep-alive
                         id="People">
                         <div class="columns is-centered">
                             <div class="column is-two-thirds">
-                                <people
-:id="companyId"
+                                <people :id="companyId"
                                     @update="$set(count, 'People', $refs.people.count)"
                                     @remove="personRemoved"
                                     ref="people"/>
                             </div>
                         </div>
                     </tab>
-                    <tab
-keep-alive
+                    <tab keep-alive
                         id="Addresses">
                         <div class="columns is-centered">
                             <div class="column is-two-thirds">
-                                <addresses
-:id="companyId"
+                                <addresses :id="companyId"
                                     type="company"
                                     @update="$set(count, 'Addresses', $refs.addresses.count)"
                                     ref="addresses"/>
